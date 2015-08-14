4f15e10254d828f34596f76c0ea48630406db7d0
Christoffer Moesgaard Albertsen <xpv919@alumni.ku.dk>
2015-08-14 15:45:09 +0200

    Fixed problem with atan2

--- src/convenience/stepbearing.hpp
+++ src/convenience/stepbearing.hpp
@@ -17,8 +17,47 @@ Type stepLength(Type x0, Type y0, Type x1, Type y1, bool nautical = true){
   return sqrt(sum(pow(dx,Type(2.0))));
 }
 
+
 template<class Type>
-Type bearing (Type x0, Type y0, Type x1, Type y1, bool nautical = true){
+Type atan3(Type y, Type x){
+
+{	Type alpha;
+	Type beta;
+	Type theta;
+
+        Type zero(0.);
+        Type pi2(2. * atan(1.));
+	Type pi(2. * pi2);
+
+	Type ax = abs(x) + 1e-6;
+	Type ay = abs(y) + 1e-6;
+	
+	// if( ax > ay )
+	// 	theta = atan(ay / ax);
+	// else	theta = pi2 - atan(ax / ay);
+	alpha = atan(ay / ax);
+	beta  = pi2 - atan(ax / ay);
+	theta = CppAD::CondExpGt(ax, ay, alpha, beta);         // use of CondExp
+
+	// if( x <= 0 )
+	// 	theta = pi - theta;
+	theta = CppAD::CondExpLe(x, zero, pi - theta, theta);  // use of CondExp
+	
+	// if( y <= 0 )
+	// 	theta = - theta;
+	theta = CppAD::CondExpLe(y, zero, -theta, theta);      // use of CondExp
+
+	// if( x == y == 0) <=> if( ax+ay == 0)
+	// theta = 0
+	theta = CppAD::CondExpEq(ax+ay, zero, zero, theta);
+	
+	return theta;
+}
+
+}
+
+template<class Type>
+Type bearing(Type x0, Type y0, Type x1, Type y1, bool nautical = true){
 
     vector<Type> dx(2);
   
@@ -29,7 +68,7 @@ Type bearing (Type x0, Type y0, Type x1, Type y1, bool nautical = true){
     dx = ll2n(x1,y1) - ll2n(x0,y0);
   }
 
-  return atan2(dx(1),dx(0));
+  return atan3(dx(1),dx(0));
 }
 
 #endif
