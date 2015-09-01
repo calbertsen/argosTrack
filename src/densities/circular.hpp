06609d78c486d056f42dddcd16f66d060adf45b8
Christoffer Moesgaard Albertsen <xpv919@alumni.ku.dk>
2015-09-01 11:27:30 +0200

    Try von Mises distribution

--- src/densities/circular.hpp
+++ src/densities/circular.hpp
@@ -6,16 +6,25 @@
     \param rho Dispersion parameter
 */
 template <class Type>
-Type dwcauchy(Type x, Type mu, Type gamma, int give_log){
-  Type log_res = log(sinh(gamma)) - log(2*M_PI) - log(cosh(gamma)-cos(x-mu));
+Type dwcauchy(Type x, Type mu, Type gamma, int give_log = 0){
+  Type logres = log(sinh(gamma)) - log(2*M_PI) - log(cosh(gamma)-cos(x-mu));
   if(give_log){
-    return CppAD::CondExpLe(gamma,Type(0.0),Type(-INFINITY),log_res);
+    return CppAD::CondExpLe(gamma,Type(0.0),Type(-INFINITY),logres);
   }else{
-    return CppAD::CondExpLe(gamma,Type(0.0),Type(0.0),exp(log_res));
+    return CppAD::CondExpLe(gamma,Type(0.0),Type(0.0),exp(logres));
   }
 
 }
 
 VECTORIZE3_tti(dwcauchy);
+
+
+template <class Type>
+Type dvonmises(Type x, Type mu, Type kappa, int give_log = 0){
+  Type logres = kappa * cos(x - mu) - log(2*M_PI) - log(besselK(kappa, Type(0)));
+  if(give_log) return logres; else return exp(logres);
+}
+
+VECTORIZE3_tti(dvonmises);
 
 #endif
